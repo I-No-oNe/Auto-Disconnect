ece6dc5de936df886890978f76bd93c9d381afe9
I-No-oNe <novonoam76@gmail.com>
2024-09-22 20:21:44 +0300

    Version 3.0

--- src/main/java/net/i_no_am/auto_disconnect/utils/NetworkUtils.java
+++ src/main/java/net/i_no_am/auto_disconnect/utils/NetworkUtils.java
@@ -2,7 +2,7 @@ package net.i_no_am.auto_disconnect.utils;
 
 import net.i_no_am.auto_disconnect.Global;
 import net.i_no_am.auto_disconnect.config.Config;
-import net.minecraft.network.packet.s2c.common.DisconnectS2CPacket;
+import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Colors;
@@ -11,7 +11,7 @@ public class NetworkUtils implements Global {
     // Credits -> Meteor Client 🔥
     public static void disconnectPlayer(String reason) {
         MutableText text = Text.literal("§3Auto-Disconnect was triggered§r");
-        text = text.append(Text.literal("\n\n" + reason).withColor(Colors.RED));
+        text = text.append(Text.literal(Colors.RED + "\n\n" + reason));
         PlayerUtils.player().networkHandler.onDisconnect(new DisconnectS2CPacket(text));
         if (Config.autoDisable.getVal()) {
             Config.autoDisable.setVal("enable", false);
